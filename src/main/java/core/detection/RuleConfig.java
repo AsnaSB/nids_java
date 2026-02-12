13d053dc5261092dffc2ecacae435387c1edf2d1
hrishitha06 <hrishithasa@gmail.com>
2026-02-12 20:25:00 +0530

    Add RuleConfig thresholds

---
+++ src/main/java/core/detection/RuleConfig.java
@@ -0,0 +1,11 @@
+package main.java.core.detection;
+
+public class RuleConfig {
+    public class RuleConfig {
+    public static final int COUNT_THRESHOLD = 100;
+    public static final double SERROR_RATE_THRESHOLD = 0.5;
+    public static final int DST_HOST_COUNT_THRESHOLD = 200;
+    public static final double RERROR_RATE_THRESHOLD = 0.5;
+}
+
+}
