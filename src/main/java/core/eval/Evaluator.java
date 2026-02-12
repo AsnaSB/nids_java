01b116917f4c631fc3b810a6de3b4f41459c8d67
Hrishithasa <hrishithakichu@gmail.com>
2026-02-12 22:55:16 +0530

    Implement Evaluator logic

---
+++ src/main/java/core/eval/Evaluator.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Evaluator {
+
+    public static class Metrics {
+        public int TP, FP, TN, FN;
+        public double accuracy;
+    }
+
+    public Metrics evaluate(List<String> predicted, List<String> actual) {
+        Metrics metrics = new Metrics();
+
+        for (int i = 0; i < predicted.size(); i++) {
+            String p = predicted.get(i);
+            String a = actual.get(i);
+
+            if (p.equals("ATTACK") && a.equals("ATTACK")) metrics.TP++;
+            else if (p.equals("ATTACK") && a.equals("NORMAL")) metrics.FP++;
+            else if (p.equals("NORMAL") && a.equals("NORMAL")) metrics.TN++;
+            else if (p.equals("NORMAL") && a.equals("ATTACK")) metrics.FN++;
+        }
+
+        metrics.accuracy = (double)(metrics.TP + metrics.TN) / predicted.size();
+        return metrics;
+    }
+}
